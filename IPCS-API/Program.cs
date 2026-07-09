af5361355639a19dba7b9a0492e6b56e45556894
mostafizur244343 <mostafizur244343@gmail.com>
2026-07-09 17:14:00 +0600

    Apply CamelCase naming policy for JSON serialization
    Added CamelCase property naming policy for JSON serialization.

--- IPCS-API/Program.cs
+++ IPCS-API/Program.cs
@@ -82,6 +82,8 @@ builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
         options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+         // Apply CamelCase for JSON serialize 
+ options.JsonSerializerOptions.PropertyNamingPolicy = System.Text.Json.JsonNamingPolicy.CamelCase;
     });
 
 builder.Services.AddScoped<IAccountService, AccountService>();
